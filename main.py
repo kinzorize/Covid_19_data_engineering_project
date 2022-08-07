43a9ff832abc536ee380074b32573f902d2d7fcb
kenzo2017 <mustardseed19@gmail.com>
2022-08-07 17:38:56 +0300

    work-in-progress

--- main.py
+++ main.py
@@ -18,3 +18,56 @@ athena_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_KEY,
     region_name=AWS_REGION,
 )
+
+# Basically this function do is to takes the boto3 object,
+# run some queries on athena and store the output in s3
+
+Dict = {}
+
+
+def download_and_load_query_results(
+    client: boto3.client, query_response: Dict
+) -> pd.DataFrame:
+    while True:
+        try:
+            # This function only loads the first 1000 rows
+            client.get_query_results(
+                QueryExecutionId=query_response["QueryExecutionId"]
+            )
+            break
+        except Exception as err:
+            if "not yet finished" in str(err):
+                time.sleep(0.001)
+            else:
+                raise err
+    temp_file_location: str = "athena_query_result.csv"
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION,
+
+    )
+    s3_client.download_file(
+        S3_BUCKET_NAME,
+        f"{S3_OUTPUT_DIRECTORY}/{query_response['QueryExecutionId']}.csv",
+        temp_file_location,
+    )
+    return pd.read_csv(temp_file_location)
+
+
+response = athena_client.start_query_execution(
+    QueryString="SELECT * FROM enigma_jhud",
+    QueryExecutionContext={"Database": SCHEMA_NAME},
+    ResultConfiguration={
+        "OutputLocation": S3_STAGING_DIR,
+        "EncryptionConfiguration": {"EncryptionOption": "SSE_S3"}
+    },
+)
+
+
+response
+
+enigma_jhud = download_and_load_query_results(athena_client, response)
+
+enigma_jhud.head()
